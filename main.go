d7bbbeebd9f071e0eb338d74f604f5c56b7f9eb4
youwkey <youwkey@gmail.com>
2022-03-05 16:08:20 +0900

    update: update alfred-go to v1.0.0

--- main.go
+++ main.go
@@ -10,86 +10,62 @@ import (
 
 func main() {
 	sf := alfred.ScriptFilter{}
-	sf.SetEmptyTitle("No Results", "")
 	ghqPath := os.Getenv("GHQ_PATH")
 
 	if ghqPath == "" {
-		sf.Append(alfred.Item{
-			Title:    "Error",
-			Subtitle: "require workflow environment variable 'GHQ_PATH'",
-			Valid:    false,
-		})
-		sf.Output()
+		item := alfred.NewInvalidItem("Error").Subtitle("require workflow environment variable 'GHQ_PATH'")
+		sf.Items().Append(item)
+		_ = sf.Output()
 		return
 	} else if f, err := os.Stat(ghqPath); err != nil {
-		sf.Append(alfred.Item{
-			Title:    "Error",
-			Subtitle: ghqPath + " not exists",
-			Valid:    false,
-		})
-		sf.Output()
+		item := alfred.NewInvalidItem("Error").Subtitle(ghqPath + " not exists")
+		sf.Items().Append(item)
+		_ = sf.Output()
 		return
 	} else if m := f.Mode(); m.IsDir() || 0111&m.Perm() == 0 {
-		sf.Append(alfred.Item{
-			Title:    "Error",
-			Subtitle: ghqPath + " not directory",
-			Valid:    false,
-		})
-		sf.Output()
+		item := alfred.NewInvalidItem("Error").Subtitle(ghqPath + " not exec")
+		sf.Items().Append(item)
+		_ = sf.Output()
 		return
 	}
 
 	var ghqRoot string
 	if bytes, err := exec.Command(ghqPath, "root").Output(); err != nil {
-		sf.Append(alfred.Item{
-			Title:    "Error",
-			Subtitle: err.Error(),
-			Valid:    false,
-		})
-		sf.Output()
+		item := alfred.NewInvalidItem("Error").Subtitle(err.Error())
+		sf.Items().Append(item)
+		_ = sf.Output()
 		return
 	} else {
 		ghqRoot = strings.TrimRight(string(bytes), "\n")
 	}
 
 	if bytes, err := exec.Command(ghqPath, "list").Output(); err != nil {
-		sf.Append(alfred.Item{
-			Title:    "Error",
-			Subtitle: err.Error(),
-			Valid:    false,
-		})
-		sf.Output()
+		item := alfred.NewInvalidItem("Error").Subtitle(err.Error())
+		sf.Items().Append(item)
+		_ = sf.Output()
 		return
 	} else {
 		for _, v := range strings.Fields(string(bytes)) {
 			absPath := filepath.Join(ghqRoot, v)
 			repoURL := "https://" + v
-			sf.Append(alfred.Item{
-				Uid:   v,
-				Title: v,
-				Arg:   absPath,
-				Match: strings.Join(strings.Split(v, "/"), " "),
-				Type:  alfred.File,
-				Mods: map[alfred.ModKey]alfred.Mod{
-					alfred.Ctrl: {
-						Arg:      absPath,
-						Subtitle: "Browse in Terminal",
-						Icon:     alfred.IconExecutableBinary,
-					},
-					alfred.Alt: {
-						Arg:      repoURL,
-						Subtitle: "Open URL",
-						Icon:     alfred.IconBookmark,
-					},
-				},
-				Icon: &alfred.Icon{
-					Type: alfred.FileIcon,
-					Path: absPath,
-				},
-				QuicklookURL: repoURL,
-			})
+			modCtrl := alfred.NewModifier().Arg(absPath).Subtitle("Browse in Terminal").Icon(alfred.IconExecutableBinary)
+			modAlt := alfred.NewModifier().Arg(repoURL).Subtitle("Open URL").Icon(alfred.IconBookmark)
+			item := alfred.
+				NewItem(v).
+				UID(v).
+				Arg(absPath).
+				Match(strings.Join(strings.Split(v, "/"), " ")).
+				Type(alfred.ItemTypeFile).
+				Mods(alfred.NewModifiers().Ctrl(modCtrl).Alt(modAlt)).
+				Icon(alfred.NewIconWithType(absPath, alfred.IconTypeFileIcon)).
+				QuicklookURL(repoURL)
+
+			sf.Items().Append(item)
+		}
+		if sf.Items().Length() == 0 {
+			sf.Items().Append(alfred.NewInvalidItem("No Results"))
 		}
 	}
 
-	sf.Output()
+	_ = sf.Output()
 }
